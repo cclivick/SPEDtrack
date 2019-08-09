3064bfe8333ff85f023c34b7ff75ddb41a0d1522
cclivick <46388110+cclivick@users.noreply.github.com>
2019-08-09 14:34:12 -0500

    Added body-parser to server file and created SPED layouts for handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,23 @@
+var path = require("path");
+
 module.exports = function(app) {
     app.get("/", function(req,res) {
         res.render("index")
     });
-}
+    
+    app.get("/teacher-login", function(req,res) {
+        res.render("teacher-login")
+    });
+
+    app.get("/parent-login", function(req,res) {
+        res.render("parent-login")
+    });
+
+    app.get("/SPED-login", function(req,res) {
+        res.render("SPED-login");
+    })
+
+    app.get("/educator-dashboard", function(req,res) {
+        res.render("educator-dashboard")
+    });
+};
--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 var express = require("express")
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var app = express();
+
 require("./routes/htmlRoutes.js")(app);
 
-var app = express();
 
 var PORT = process.argv.PORT || 8080
 
 app.use(express.static('public'))
+app.use(bodyParser.static('public'))
 
 app.use(express.urlencoded({ extended:true }))
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ limit:false }))
+app.use(bodyParser.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
 
+  app.get("/", function(req,res) {
+    res.render("index")
+});
+
 module.exports = app;
+
