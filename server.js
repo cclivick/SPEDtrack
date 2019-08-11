4caefa755f39681fa87b2052fe65a53500032451
cclivick <46388110+cclivick@users.noreply.github.com>
2019-08-10 20:07:20 -0500

    Added studentModel and mongoose instead of MySQL for database construction

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require("express")
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 var app = express();
 
 require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 
 var PORT = process.argv.PORT || 8080
+
+
+app.use(logger("dev"));
 
 app.use(express.static('public'))
 app.use(bodyParser.static('public'))
@@ -20,13 +26,12 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+mongoose.connect("mongodb://localhost/SPEDtrack", { useNewUrlParser: true });
+
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
-
-  app.get("/", function(req,res) {
-    res.render("index")
-});
 
 module.exports = app;
 
